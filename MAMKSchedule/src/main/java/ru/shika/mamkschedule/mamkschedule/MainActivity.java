106c7afb49ebc04f764e86c7b1b0d73c17683023
ShikaSD <bezobraznos@gmail.com>
2014-11-08 21:14:53 +0200

    Added cards and Fragment

--- MAMKSchedule/src/main/java/ru/shika/mamkschedule/mamkschedule/MainActivity.java
+++ MAMKSchedule/src/main/java/ru/shika/mamkschedule/mamkschedule/MainActivity.java
@@ -92,9 +92,11 @@ public class MainActivity extends ActionBarActivity {
         }
 
         @Override
-        public CharSequence getPageTitle(int position) {
+        public CharSequence getPageTitle(int position)
+        {
             Locale l = Locale.getDefault();
-            switch (position) {
+            switch (position)
+            {
                 case 0:
                     return getString(R.string.title_section1).toUpperCase(l);
                 case 1:
@@ -103,39 +105,6 @@ public class MainActivity extends ActionBarActivity {
                     return getString(R.string.title_section3).toUpperCase(l);
             }
             return null;
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_schedule, container, false);
-            return rootView;
         }
     }
 
---
+++ MAMKSchedule/src/main/java/ru/shika/mamkschedule/mamkschedule/ScheduleFragment.java
@@ -0,0 +1,8 @@
+package ru.shika.mamkschedule.mamkschedule;
+
+/**
+ * Created by NOTE on 08.11.2014.
+ */
+public class ScheduleFragment
+{
+}
